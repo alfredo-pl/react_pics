b8c288f29514fccee59ef04be9cc0698e8323578
Alfredo Peña <aalfredoplz@gmail.com>
2021-06-29 11:00:12 -0400

    add input and style to  component SearchBar

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,10 @@ import SearchBar from './SearchBar/SearchBar';
 
 const App = () => {
     return (
-        <div><SearchBar/></div>
-      );
+        <div className="ui container" style={{marginTop:'10px'}}>
+            <SearchBar/>
+        </div>
+    );
 }
  
 export default App;
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,6 +2,15 @@ import React from 'react';
 
 export default class SearchBar extends React.Component {
     render(){
-        return <div>Search Bar</div>
+        return( 
+            <div className="ui segment">
+                <form className="ui form">
+                    <div className="field">
+                        <label>Image Search</label>
+                        <input type="text"/>
+                    </div>
+                </form>   
+            </div>
+        )
     }
 };
