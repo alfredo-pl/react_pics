022612f5b59237719e5e3415ff2dcaef3c816aa3
Alfredo Peña <aalfredoplz@gmail.com>
2021-07-03 17:31:35 -0400

    setting state after async request

--- src/components/App.js
+++ src/components/App.js
@@ -4,20 +4,23 @@ import SearchBar from './SearchBar/SearchBar';
 
 
 export default class App extends React.Component {
-    
-    onSearchSubmit(term) {
-        axios.get('https://api.unsplash.com/search/photos',{
+    state = { images:[]};
+    onSearchSubmit= async term => {
+        const response = await axios.get('https://api.unsplash.com/search/photos',{
             params:{ query: term},
             headers:{
                 Authorization: `${process.env.REACT_APP_CLIENT_ID}`
             }
-        })
+        });
+        
+        this.setState({ images: response.data.results});
     }
     
     render(){
         return (
             <div className="ui container" style={{marginTop:'10px'}}>
                 <SearchBar onSubmit={this.onSearchSubmit}/>
+                Fount: {this.state.images.length} images
             </div>
         );
     }
