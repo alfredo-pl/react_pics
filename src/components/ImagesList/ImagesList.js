80a79c3dc81ed497ed4c72f4c242606f6739060f
Alfredo Peña <aalfredoplz@gmail.com>
2021-07-04 11:02:41 -0400

    method to go through the props of images

--- src/components/ImagesList/ImagesList.js
+++ src/components/ImagesList/ImagesList.js
@@ -1,6 +1,11 @@
 import React from 'react';
 const ImagesList = (props) => {
-    return <div>Images List</div>
+
+    const images = props.images.map(({description, urls,id})=>{
+        return <img key={id } src={urls.regular} alt={description} />
+    })
+
+    return <div>{images}</div>
 };
  
 export default ImagesList;
